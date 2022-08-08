9ee724670bdef9f1f27223248bbc3c9c190b1b8d
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-08 10:49:51 +0500

    add task 003 Day - решение задачи - день недели

---
+++ task003_Day/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3
+Console.Write("Введите число от 1 до 7: ");
+
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+  Console.WriteLine($"{day} -> Monday");
+}
+else if (day == 2)
+{
+  Console.WriteLine($"{day} -> Tuesday");
+}
+else if (day == 3)
+{
+  Console.WriteLine($"{day} -> Wednesday");
+}
+else if (day == 4)
+{
+  Console.WriteLine($"{day} -> Thursday");
+}
+else if (day == 5)
+{
+  Console.WriteLine($"{day} -> Friday");
+}
+else if (day == 6)
+{
+  Console.WriteLine($"{day} -> Saturday");
+}
+else if (day == 7)
+{
+  Console.WriteLine($"{day} -> Sunday");
+}
