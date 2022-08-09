2036a93ecea085bbfe58870794bb80693acc7f4b
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-09 09:44:40 +0500

    исправила после проверки while -> for

--- task008_Even_1_to_N/Program.cs
+++ task008_Even_1_to_N/Program.cs
@@ -6,12 +6,9 @@
 Console.Write("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
 
-int i = 2;
-
 Console.Write($"{N} -> ");
 
-while (i <= N)
+for (int i = 2; i <= N; i=i+2) 
 {
   Console.Write($"{i} ");
-  i = i + 2;
 }
