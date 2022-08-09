07d6a9f162690ddcfa772008a6cc7947a6cf3b70
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-09 09:35:56 +0500

    исправила после проверки if-else -> switch-case

--- task003_Day/Program.cs
+++ task003_Day/Program.cs
@@ -3,31 +3,27 @@ Console.Write("Введите число от 1 до 7: ");
 
 int day = int.Parse(Console.ReadLine());
 
-if (day == 1)
+switch(day)
 {
-  Console.WriteLine($"{day} -> Monday");
-}
-else if (day == 2)
-{
-  Console.WriteLine($"{day} -> Tuesday");
-}
-else if (day == 3)
-{
-  Console.WriteLine($"{day} -> Wednesday");
-}
-else if (day == 4)
-{
-  Console.WriteLine($"{day} -> Thursday");
-}
-else if (day == 5)
-{
-  Console.WriteLine($"{day} -> Friday");
-}
-else if (day == 6)
-{
-  Console.WriteLine($"{day} -> Saturday");
-}
-else if (day == 7)
-{
-  Console.WriteLine($"{day} -> Sunday");
+  case 1: 
+    Console.WriteLine($"{day} -> Monday");
+    break;
+  case 2:
+    Console.WriteLine($"{day} -> Tuesday");
+    break;
+  case 3:
+    Console.WriteLine($"{day} -> Wednesday");
+    break;
+  case 4:
+    Console.WriteLine($"{day} -> Thursday");
+    break;
+  case 5:
+    Console.WriteLine($"{day} -> Friday");
+    break;
+  case 6:
+    Console.WriteLine($"{day} -> Saturday");
+    break;
+  case 7:
+    Console.WriteLine($"{day} -> Sunday");
+    break;
 }
