1c2d1e5acec1e79aed2db537dacc431fb75190d6
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-09 21:56:55 +0500

    rename tast001 -> task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Задача 1
 // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
 
-Console.Write("Введите число a: ");
-int a = int.Parse(Console.ReadLine());
+  Console.Write("Введите число a: ");
+  int a = int.Parse(Console.ReadLine());
 
-Console.Write("Введите число b: ");
-int b = int.Parse(Console.ReadLine());
+  Console.Write("Введите число b: ");
+  int b = int.Parse(Console.ReadLine());
 
 
 if(a == b*b) {
