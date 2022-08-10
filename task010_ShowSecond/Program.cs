7e822b5f6121c1c498960a98a02e881e880c992c
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-10 11:22:37 +0500

    add task 010 Show second digit

--- task010_ShowSecond/Program.cs
+++ task010_ShowSecond/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int num2 = number % 100 / 10;
+Console.WriteLine($"{number} -> {num2}");
