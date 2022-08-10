0c95c1c19d0864dbc5e259375d3844d53b25796d
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-10 14:58:40 +0500

    add task 015 Holiday

---
+++ task015_Holiday/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите число: ");
+int day = int.Parse(Console.ReadLine());
+
+if (0 < day && day < 6)
+{
+  Console.WriteLine($"{day} -> no");
+}
+else if (5 < day && day < 8)
+{
+  Console.WriteLine($"{day} -> yes");
+}
+else
+{
+  Console.WriteLine("Введено некоректное число");
+}
+
+
+// switch (day)
+// {
+//   case 1:
+//     Console.WriteLine($"{day} -> no");
+//     break;
+//   case 2:
+//     Console.WriteLine($"{day} -> no");
+//     break;
+//   case 3:
+//     Console.WriteLine($"{day} -> no");
+//     break;
+//   case 4:
+//     Console.WriteLine($"{day} -> no");
+//     break;
+//   case 5:
+//     Console.WriteLine($"{day} -> no");
+//     break;
+//   case 6:
+//     Console.WriteLine($"{day} -> yes");
+//     break;
+//   case 7:
+//     Console.WriteLine($"{day} -> yes");
+//     break;
+//     default:
+//       Console.WriteLine("Введено некоректное число");
+//       break;
+// }
