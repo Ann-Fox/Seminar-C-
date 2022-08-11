18f6409979ab9cf5ef4a335207117fd4b5f849ea
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-11 11:03:07 +0500

    task 013 решила задачу!!!

--- task013_ThirdDigit/Program.cs
+++ task013_ThirdDigit/Program.cs
@@ -1,18 +1,38 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Строки и циклы в решении использовать нельзя!)
-// 645 -> 5
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Строки и циклы в решении использовать нельзя!). Цифры подсчитываются справа налево
+// 645 -> 6
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
-if (number < 100) {
-Console.WriteLine($"{number} -> третьей цифры нет");
+
+if (number <= 99)
+{
+  Console.WriteLine($"{number} -> третьей цифры нет");
 }
 
-while(number > 1000) {
-  number = number / 10;
-  Console.WriteLine(number);
+if (99 < number && number < 1000)
+{
+  Console.WriteLine($"{number} -> {number / 100}");
+}
+
+if (number > 999)
+{
+  int n321 = number / 100;
+  int n3 = n321 % 10;
+  Console.WriteLine($"{number} -> {n3}");
+}
+
+
+// if (number < 100) {
+// Console.WriteLine($"{number} -> третьей цифры нет");
+// }
+// while(number => 1000) {
+//   number = number / 10;
+//   Console.WriteLine(number);
+
+
 
 // if (99 < number && number< 1000)
 //  {
@@ -24,9 +44,7 @@ while(number > 1000) {
 // Console.WriteLine($"{number} -> третьей цифры неt}");
 // }
 
-}
 
-
-Console.WriteLine($"{number} -> {number % 10}");
+// Console.WriteLine($"{number} -> {number % 10}");
 
 
