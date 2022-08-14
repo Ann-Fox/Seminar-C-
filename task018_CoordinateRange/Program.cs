14ac3d89fb049d8a2a4c12733b1d1de9e8312f3c
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-14 12:46:34 +0500

    add task 018 Coordinate Range(if else, swithc-case)  Seminar 3

---
+++ task018_CoordinateRange/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+//2/1
+//3/4
+
+Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+
+// if (number == 1) {
+//   Console.Write("x > 0, y > 0");
+//   }
+// else if (number == 2) {
+//   Console.Write("x < 0, y > 0");
+//   }
+// else if (number == 3) {
+//   Console.Write("x < 0, y < 0");
+//   }
+// else if (number == 4) {
+//   Console.Write("x > 0, y < 0");
+//   } 
+// else {
+//   Console.WriteLine("Такой четверти нет");
+//   }
+
+switch (number)
+{
+  case (1):
+    Console.Write("x > 0, y > 0");
+    break;
+  case (2):
+    Console.Write("x < 0, y < 0");
+    break;
+  case (3):
+    Console.Write("x < 0, y < 0");
+    break;
+  case (4):
+    Console.Write("x > 0, y < 0");
+    break;
+  default:
+    Console.WriteLine("Такой четверти нет");
+    break;
+}
