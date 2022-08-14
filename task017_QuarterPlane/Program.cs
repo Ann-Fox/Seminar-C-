eabee9f24d6c8941da89adc8ef37288b3e1bfdc2
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-14 12:15:29 +0500

    add task 017 Quater plane Seminar 3

---
+++ task017_QuarterPlane/Program.cs
@@ -0,0 +1,14 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+// image.png
+// четверти 2/1
+//          3/4
+
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.Write("1");
+if (x < 0 && y > 0) Console.Write("2");
+if (x < 0 && y < 0) Console.Write("3");
+if (x > 0 && y < 0) Console.Write("2");
