d2dcae190d6d405eb930eb7503c50b72bd2b47c6
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-19 21:39:00 +0500

    task 24, 26, 28, 30 Seminar 4

---
+++ task026_Count/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// int count = 0;
+// int num = n;
+
+// while (n > 0)
+// {
+//   count++;
+//   n = n / 10;
+// }
+// Console.Write($" {num} -> {count}");
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int DiditCount(int a)
+{
+  int count = 0;
+  while (a > 0)
+  {
+    count++;
+    a /= 10;
+  }
+  return count;
+
+}
+Console.Write(DiditCount(num));
