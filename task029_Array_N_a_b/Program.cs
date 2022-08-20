09b2edc12d5db2b7cb90fa17e00d1a361f5b81c5
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-20 15:28:35 +0500

    added task 029 array N-element [a,b) Homeworke4

---
+++ task029_Array_N_a_b/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) и выводит их на экран.
+// 5, 0, 20 -> [1, 2, 5, 7, 19]
+// 3, 1, 35 -> [6, 1, 33]
+
+void RandArray(int size, int a, int b)
+{
+  int[] array = new int[size];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(a, b);
+  }
+  Console.WriteLine($"{size}, {a}, {b} -> [{String.Join(",", array)}]");
+}
+
+RandArray(5, 0, 20);
+RandArray(3, 1, 35);
+RandArray(5, 1, 7);
