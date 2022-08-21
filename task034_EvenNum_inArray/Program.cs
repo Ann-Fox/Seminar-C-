3f42ba6de6697b2d9baf989617831c0b5e30622d
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-21 12:01:48 +0500

    added task 034 even num in array Homework5

---
+++ task034_EvenNum_inArray/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] Array(int size)
+{
+  int[] array = new int[size];
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(100, 1000);
+  }
+  // Console.WriteLine($"[{String.Join(", ", array)}]"); //вывод массива
+  return array;
+}
+
+
+int EvenNumArray(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] % 2 == 0)
+    {
+      count++;
+    }
+  } 
+  Console.WriteLine($"[{String.Join(", ", arr)}] -> {count}");
+  return count;
+
+}
+
+EvenNumArray(Array(5));
+EvenNumArray(Array(6));
+
