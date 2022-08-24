3a983ff978d0ed163694cf0aab427295244572e4
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-08-24 10:40:50 +0500

    added tasks 039, 040, 042, 044, 045 Seminar 6

---
+++ task040_TriangleYesNo/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+
+bool CheckTriangle(int n1, int n2, int n3)
+{
+  return n1 + n2 > n3 && n1 + n3 > n2 && n2 + n3 > n1;
+}
+
+if (CheckTriangle(a, b, c))
+{
+  Console.Write("Yes");
+}
+else
+{
+  Console.Write("No");
+}
+}
+
+//  CheckTriangle(3, 4, 5); что-то пошло не так, пересмотреть
---
+++ task045_CopyArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int[] GetArray(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  Console.WriteLine(res);
+
+  return res;
+}
+
+
+int[] GetCopy(int[] array)
+{
+  int[] result = new int[array.Length];
+  for (int i = 0; i < array.Length; i++)
+  {
+    result[i] = array[i];
+  }
+  return result;
+}
+
+int[] myArray = GetArray(5, 0, 10);
+Console.WriteLine(String.Join(" ", GetCopy(myArray)));
