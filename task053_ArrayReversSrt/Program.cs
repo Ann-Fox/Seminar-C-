5ed41c9a9373090b0773fef344ac542d81ea0235
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-09-01 20:58:45 +0500

    added tasks 053, 055, 057, 059 Seminar 8

---
+++ task053_ArrayReversSrt/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+  return result;
+}
+
+void CorrectArray(int[,] array)
+{
+  int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+  int temp;
+
+  for (int i = 0; i < array.GetLength(1); i++)
+
+  //0,0  0,1  0,2  0,3
+  //
+  //5,0  5,1  5,2  5,3
+
+  {
+    temp = array[0, i];
+    array[0, i] = array[array.GetLength(0) - 1, i];
+    array[array.GetLength(0) - 1, i] = temp;
+  }
+ }
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+  }
+}
+Console.Write("m= ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n= ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(m, n, 0, 10);
+
+PrintArray(myArray);
+CorrectArray(myArray);
+
+Console.WriteLine();
+PrintArray(myArray);
---
+++ task055_ArrayStrColum/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+  return result;
+}
+
+void CorrectArray(int[,] array)
+{
+  int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+
+  for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+
+      //0,0  0,1  0,2      0,0  1,0  2,0
+      //1,0  1,1  1,2      0,1  1,1  2,1
+      //2,0  2,1  2,2      0,2  1,2  2,2
+
+      if (array.GetLength(0) == array.GetLength(1))
+      {
+        result[i,j] = array[j, i];
+      }
+      else
+      {
+        Console.Write("Error");
+      }
+
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+  }
+}
+Console.Write("m= ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n= ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(m, n, 0, 10);
+
+PrintArray(myArray);
+CorrectArray(myArray);
+
+Console.WriteLine();
+PrintArray(myArray);
+
+// int[,] NewArray(int[,] array)
+// {
+//     int[,] tmp = new int[array.GetLength(0), array.GetLength(1)];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             tmp[i, j] = array[j, i];
+//         }
+//     }
+//     return tmp;
+// }
+
+// int[,] myArray = GetArray(m, n);
+// PrintArray(myArray);
+// Console.WriteLine();
+// if (myArray.GetLength(0) == myArray.GetLength(1))
+// {
+//     PrintArray(NewArray(myArray));
+// }
+// else
+//     Console.Write("Кол-во столбцов не совпадает с кол-во строк!");
