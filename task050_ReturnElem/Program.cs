d4f6a19867db24ab213bfc9f4ace2ee22fe711c6
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-09-02 16:03:59 +0500

    added tasks 047, 050, 052 Homework7

---
+++ task050_ReturnElem/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// m = 1, n =7 -> такого числа в массиве нет
+
+Console.Write("Введите номер строки row = ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Введите номер столбца col = ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+  return result;
+}
+
+void PrintArray(int[,] array)
+{
+
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+  }
+}
+
+// Функция ничего не возвращает, не удалось реализовать данное условие "возвращает значение этого элемента или же указание, что такого элемента нет"
+//  Разный вид возвращаемых значений, как преобразовать?
+
+void SearchElement(int[,] arr)
+{
+  if (row > arr.GetLength(0) || col > arr.GetLength(1))
+  {
+    Console.Write("такого числа в массиве нет");
+  }
+
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+      if (row == i && col == j)
+        Console.Write(arr[row, col]);
+  }
+}
+
+Console.WriteLine();
+int[,] myArray = GetArray(3, 4, 0, 10);
+PrintArray(myArray);
+Console.Write($"row = {row}, col = {col} -> ");
+SearchElement(myArray);
---
+++ task052_ArithmrticElemColumn/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+  return result;
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write($"{array[i, j]} ");
+    Console.WriteLine();
+  }
+}
+
+void ArithmeticMeanColumn(int[,] array)
+{
+  int[] summ = new int[array.GetLength(1)];
+  double arithmeticSum = 1;
+
+  for (int i = 0; i < array.GetLength(1); i++)
+  {
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+      summ[i] += array[j, i];
+    }
+    arithmeticSum = Math.Round(summ[i] * 1.0 / array.GetLength(0), 1);
+    Console.Write($"{arithmeticSum}  ");
+  }
+}
+
+int[,] myArray = GetArray(3, 5, 0, 10);
+PrintArray(myArray);
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+ArithmeticMeanColumn(myArray);
